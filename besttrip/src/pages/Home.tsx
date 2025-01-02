334481709faf07c2b80b4b835cdc783088fbfb4d
minb__m <kmb3322@naver.com>
2025-01-02 20:39:45 +0900

    초기

--- besttrip/src/pages/Home.tsx
+++ besttrip/src/pages/Home.tsx
@@ -1,8 +1,4 @@
-import {
-  Box,
-  Flex,
-  Text
-} from "@chakra-ui/react";
+import { Box, Flex, Text } from "@chakra-ui/react";
 import { useRef, useState } from "react";
 import BottomTabBar from "../components/BottomTabBar";
 import GroupDetail from "../components/GroupDetail";
@@ -15,32 +11,46 @@ const mockGroups: Group[] = [
     id: 1,
     name: "친구들과의 제주도 여행",
     nickname: "제주불주먹",
-    coverImage:
-      "https://images.unsplash.com/photo-1523301343968-6a6ebf61f8ce?auto=format&q=80&w=800",
+    // 표지 사진 (coverImage)
+    coverImage: "/images/image1.jpg",
     members: ["김철수", "이영희", "박영수"],
     places: ["제주도", "우도", "성산일출봉"],
     dates: ["2024-10-12 ~ 2024-10-15"],
+    // 실제 더미 갤러리 사진들
     galleryImages: [
-      "https://images.unsplash.com/photo-1605175997171-8a2b41fc2603?auto=format&q=80&w=800",
-      "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?auto=format&q=80&w=800",
-      "https://images.unsplash.com/photo-1473625341700-0ebc3d13bb1f?auto=format&q=80&w=800",
-      // ...
+      "/images/image2.jpg",
+      "/images/image3.jpg",
+      "/images/image4.jpg",
     ],
   },
   {
     id: 2,
     name: "가족 여행",
     nickname: "우리집 최고",
-    coverImage:
-      "https://images.unsplash.com/photo-1542038784456-b3f2f6bdd25c?auto=format&q=80&w=800",
+    coverImage: "/images/image5.jpg",
     members: ["김아빠", "김엄마"],
     places: ["강원도", "주문진", "속초"],
     dates: ["2025-01-01 ~ 2025-01-05"],
     galleryImages: [
-      // ...
+      "/images/image6.jpg",
+      "/images/image7.jpg",
+      "/images/image8.jpg",
     ],
   },
-  // 원하는 만큼 더미 데이터 추가
+  {
+    id: 3,
+    name: "회사 워크샵",
+    nickname: "열정 가득 우리팀",
+    coverImage: "/images/image9.jpg",
+    members: ["홍길동", "김코딩", "박해커"],
+    places: ["서울", "분당", "수원"],
+    dates: ["2025-03-10 ~ 2025-03-12"],
+    galleryImages: [
+      "/images/image10.jpg",
+      "/images/image11.jpg",
+      "/images/image12.jpg",
+    ],
+  },
 ];
 
 export default function Home() {
